fb614792e0c6b9f4401703ec79308a7ba6de5a0f
sakshatshinde <shindesakshat@gmail.com>
2023-03-08 18:59:15 +0530

    Broke code down into structures, added documentation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use human_bytes::human_bytes;
-use std::{thread, time::Duration};
-use sysinfo::{CpuRefreshKind, ProcessExt, ProcessRefreshKind, RefreshKind, System, SystemExt};
+use sysinfo::{
+    CpuRefreshKind, PidExt, ProcessExt, ProcessRefreshKind, RefreshKind, System, SystemExt,
+};
 
 fn main() {
     // ? Creating a global system
@@ -11,40 +12,62 @@ fn main() {
             .with_processes(ProcessRefreshKind::everything()),
     );
 
-    loop {
-        // ? Only refreshing what we requested
-        sys.refresh_specifics(
-            RefreshKind::new()
-                .with_memory()
-                .with_cpu(CpuRefreshKind::everything())
-                .with_processes(ProcessRefreshKind::everything()),
-        );
+    let l_process = get_process_info(&mut sys, "firefox.exe");
+    println!("{:?}", l_process);
+}
 
-        for process in sys.processes_by_exact_name("qbittorrent.exe") {
-            println!(
-                "\n pid: {} \n name: {} \n cpu: {}% \n ram: {} \n virt memory: {}",
-                process.pid(),
-                process.name(),
-                process.cpu_usage(),
-                human_bytes(process.memory() as f64),
-                human_bytes((process.virtual_memory()) as f64),
-            );
-        }
-        thread::sleep(Duration::from_millis(4000));
+/// Returns a vec of processes, usually only one process is included but sometimes multiple processes can
+/// spawn with the same name. Like chrome spawing its child process for every tab
+/// ### Arguments
+///
+/// * `sys` - The system initialized using `sysinfo::new()` or any of its variants
+/// * `process_exact_name` - The exact name of the process, on Windows with `.exe`
+fn get_process_info(sys: &mut System, process_exact_name: &str) -> Vec<ProcessInfo> {
+    // ? Only refreshing what we requested
+    sys.refresh_specifics(
+        RefreshKind::new()
+            .with_memory()
+            .with_cpu(CpuRefreshKind::everything())
+            .with_processes(ProcessRefreshKind::everything()),
+    );
+
+    let mut vec_p_info = Vec::<ProcessInfo>::new();
+
+    for process in sys.processes_by_exact_name(process_exact_name) {
+        let p_info = ProcessInfo {
+            pid: process.pid().as_u32(),
+            name: process.name().to_string(),
+            cpu_usage: process.cpu_usage(),
+            ram_usage: human_bytes(process.memory() as f64),
+            virtual_memory_usage: human_bytes((process.virtual_memory()) as f64),
+        };
+
+        vec_p_info.push(p_info);
     }
+    vec_p_info
+}
+
+/// ProcessInfo is a struct to just keep track of a singular process
+#[derive(Debug)]
+struct ProcessInfo {
+    pid: u32,
+    name: String,
+    cpu_usage: f32,
+    ram_usage: String,
+    virtual_memory_usage: String,
 }
 
 fn core_to_core_latency() {
-    !todo!()
+    todo!()
 }
 
 fn get_amd_gpu_info() {
-    !todo!()
+    todo!()
     // ! machine_info: https://docs.rs/machine-info/latest/machine_info/
 }
 
 fn get_data_bandwidth_ram_gpu() {
-    !todo()
+    todo!()
     // ! ufe_abe: https://lib.rs/crates/ufe_abe
 }
 // * https://www.reddit.com/r/windows/comments/10p6ea0/can_i_have_some_help_understanding_memory_values/
@@ -54,3 +77,24 @@ Working set is the amount of private memory that the OS has currently assigned t
 
 The Processes tab of Task Manager shows private working set for each program, but the total at the top includes private working sets and shared memory. Shared memory doesn't have a line item and thus confusingly the line items don't add up to the total. The Details tab should show you a lot of the same data as Process Explorer if you enable the columns, though perhaps with different names.
  */
+
+// println!(
+//     "\n pid: {} \n name: {} \n cpu: {}% \n ram: {} \n virt memory: {}",
+//     process.pid(),
+//     process.name(),
+//     process.cpu_usage(),
+//     human_bytes(process.memory() as f64),
+//     human_bytes((process.virtual_memory()) as f64),
+// );
+
+// impl ProcessInfo {
+//     fn default() -> ProcessInfo {
+//         ProcessInfo {
+//             pid: 0,
+//             name: String::from("default"),
+//             cpu_usage: 0.0,
+//             ram_usage: String::from("default"),
+//             virtual_memory_usage: String::from("default"),
+//         }
+//     }
+// }
